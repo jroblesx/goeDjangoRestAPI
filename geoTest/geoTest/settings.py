0b7f4d21f9a1cfb31778cbead15b7ebc435abc66
Jean Robles <jeanr.robles@gmail.com>
2017-06-02 14:12:01 -0400

    Test Mozio

--- geoTest/geoTest/settings.py
+++ geoTest/geoTest/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'world',
- 
+    'rest_framework',
+    'drf_autodocs',
+    'provider',
 )
 
 MIDDLEWARE_CLASSES = (
